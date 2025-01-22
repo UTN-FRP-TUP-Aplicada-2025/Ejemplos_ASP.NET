452d834bd7b45ba689a81e0fc64686ad9cbe6dc5
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2025-01-21 23:11:59 -0300

    .

--- Ejemplo_ASP.NET_MVC6/Ejemplo_10_form_dinamico/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ejemplo_10_form_dinamico.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

e78cfd982edff34940e478eb9c475e5839000f17
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2025-03-03 18:24:43 -0300

    refactorizando proyecto

--- Ejemplos_ASP.NET/ASP.NET.MVC/Ejemplo_07_Login_Identity/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ejemplo_07_Login_Identity.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Ejemplos_ASP.NET/Propiedades/Ejemplo1/TransaccionalAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejemplo1;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class TransaccionalAttribute : Attribute
+{
+    public int Nivel { get; }
+
+    public TransaccionalAttribute(int nivel)
+    {
+        Nivel = nivel;
+    }
+}

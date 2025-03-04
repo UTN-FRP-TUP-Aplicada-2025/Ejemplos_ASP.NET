7f243d1017af3dd185ee9f3a64cfea02ee5afc74
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2025-03-04 15:36:50 +0000

    incluyendo otros scripts

--- Ejemplos_ASP.NET/CONTENEDORES/scripts/clean_all.sh
+++ Ejemplos_ASP.NET/CONTENEDORES/scripts/clean_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#detiene y elimina todos los contenedores
+# detiene y elimina todos los contenedores
 docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
 
 # elimina todas las imagenes
@@ -14,3 +14,8 @@ docker network prune -f
 
 # elimina todo lo no no utilizado: contenedores, imágenes, volúmenes y redes 
 docker system prune -a --volumes -f
+
+# limpia recursos no utilizados. docker system prune
+# -a          elimina imágenes sin referencia.
+# --volumes   incluye volúmenes no utilizados.
+# -f          borra TODO sin confirmación.

29e2a39d9d8e5c960000eca631d93ed2694fb70c
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2025-03-22 10:17:09 -0300

    upload

---
+++ Ejemplos_ASP.NET/ASP.NET.BLAZOR.WEBAPP/Ejemplo_07/Ejemplo_FileUpload/Controllers/UploadController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejemplo_FileUpload.Controllers;
+
+[ApiController]
+[Route("api/upload")]
+public class UploadController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> UploadFiles([FromForm] List<IFormFile> files)
+    {
+        if (files == null || files.Count == 0)
+            return BadRequest("No se han enviado archivos.");
+
+        foreach (var file in files)
+        {
+            var filePath = Path.Combine("Uploads", file.FileName);
+
+            using var stream = new FileStream(filePath, FileMode.Create);
+            await file.CopyToAsync(stream);
+        }
+
+        return Ok("Archivos subidos correctamente.");
+    }
+}
--- Ejemplos_ASP.NET/ASP.NET.BLAZOR.WEBAPP/Ejemplo_07/Ejemplo_FileUpload/wwwroot/js/filePaste.js
+++ Ejemplos_ASP.NET/ASP.NET.BLAZOR.WEBAPP/Ejemplo_07/Ejemplo_FileUpload/wwwroot/js/filePaste.js
@@ -1,12 +1,13 @@
-export function initializeFilePaste(fielDropContainer, inputFile)
+export function initializeFilePaste(fileDropContainer, inputFile)
 {
     fileDropContainer.addEventListener('paste', onPaste);
+
     function onPaste(event)
     {
-        inputFile.files = event.clipboardData.files.
+        inputFile.files = event.clipboardData.files;
         const changeEvent = new Event('change', { bubbles: true });
         inputFile.dispatchEvent(changeEvent);
-    }
+    } 
 
     return {
         dispose: () => {

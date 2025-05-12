c65e51d32f85a865e36205b8bef513f1818e1a67
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2025-05-12 17:20:40 -0300

    .

---
+++ Ejemplos_ASP.NET/ASP.NET.BLAZOR.WEBAPP/Ejemplo_02/Ejemplo_02_0_CRUD_RestAPI_y_Web_Simple/RestControllers/RedirigirController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ejemplo_02_0_CRUD_RestAPI_y_Web_Simple.RestControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedirigirController : ControllerBase
+    {
+        
+        [HttpGet("Redirigir")]
+        async public Task<IActionResult> RedirigirAsync(string url)
+        {
+            return Redirect(url);
+        }
+    }
+}
